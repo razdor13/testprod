8e9670c8652c7a91d0b3b145860cc0129406e1c8
Razdor13 <oneamaday@gmail.com>
2023-10-20 13:28:16 +0300

    new app start

--- javascript.js
+++ javascript.js
@@ -1,1 +1,19 @@
-console.log("10" + 5);
+const numberOfFilms = +prompt("Cкільки фільмів ви подивились?");
+
+const personalMovieDB = {
+    count: 1,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false,
+};
+
+const a = prompt("останній фільм що дивились ?"),
+    b = prompt("як оціните його?"),
+    c = prompt("останній фільм що дивились ?");
+d = prompt("як оціните його?");
+
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+
+console.log(personalMovieDB);
