505df9578fb9b694b7767866b0fec1394055b76b
Razdor13 <oneamaday@gmail.com>
2023-10-21 11:53:37 +0300

    modificate

--- javascript.js
+++ javascript.js
@@ -1,19 +1,35 @@
-const numberOfFilms = +prompt("Cкільки фільмів ви подивились?");
-
+"use strict";
 const personalMovieDB = {
-    count: 1,
+    count: 0,
     movies: {},
     actors: {},
     genres: [],
     privat: false,
 };
 
-const a = prompt("останній фільм що дивились ?"),
-    b = prompt("як оціните його?"),
-    c = prompt("останній фільм що дивились ?");
-d = prompt("як оціните його?");
+personalMovieDB.count = +prompt("Cкільки фільмів ви подивились?");
 
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
+for (let i = 0; i < 2; i++) {
+    const a = prompt("Один з останніх фільмів що дивились ?");
+    const b = prompt("На скільки його оціните?");
+
+    if (a != null && b != null && a != "" && a.length < 50) {
+        personalMovieDB.movies[a] = b;
+        console.log("done");
+    } else {
+        console.log("error");
+        i--;
+    }
+}
+
+if (personalMovieDB.count < 10) {
+    console.log("мало");
+} else if (personalMovieDB.count > 10 && personalMovieDB < 30) {
+    console.log("непогано");
+} else if (personalMovieDB.count >= 30) {
+    console.log("Багато подивився, красень");
+} else {
+    console.log("якась помилка");
+}
 
 console.log(personalMovieDB);
